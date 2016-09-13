90eb3c218de7a4643cf5014b76589d6d4b4de1ae
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2016-09-13 08:26:36 +0200

    ...

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 
 #include "include/amici.h"
 #include "objectivefunction.h"
+#include "localoptimization.h"
 
 void error(const char *message) { // exit?
     printf("ERROR: %s\n", message);
@@ -19,31 +20,11 @@ void warning(const char *message) {
     printf("WARNING: %s\n", message);
 }
 
-void optimizeModel() {
-    bool converged = 1;
-    int curIteration = 0;
-    do {
-        ++curIteration;
-        printf("Iteration %d\n", curIteration);
-        fflush(stdout);
-
-        double timepoints [] = {};
-        double theta [] = {};
-        clock_t timeBegin = clock();
-        double j = evaluateObjectiveFunction(timepoints, theta,
-                                             NUM_FIXED_PARAMS + NUM_STATE_VARIABLES,
-                                             NUM_CELL_LINES, true);
-        printf("Objective function value %e\n", j);
-        clock_t timeEnd = clock();
-        double timeElapsed = (double) (timeEnd - timeBegin) / CLOCKS_PER_SEC;
-        printf("Elapsed time %fs\n", timeElapsed);
-    } while(!converged);
-}
 
 int main(int argc, char **argv)
 {
-    optimizeModel();
-    printf("Test\n");
+    double initialTheta[NUM_OPTIMIZATION_PARAMS] = {0};
+    getLocalOptimum(initialTheta);
 }
 
 
