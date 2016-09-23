a4bb18ff009dcecf7d1388d53d01b92c3e8ad002
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2016-09-23 08:30:18 +0200

    MPI includes and libs

--- main.c
+++ main.c
@@ -3,17 +3,29 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <time.h>
+#include <mpi.h>
 
 #include "localoptimization.h"
 #include "objectivefunction.h"
 
 int main(int argc, char **argv)
 {
+    int commSize, rank;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &commSize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    printf(" I am %d of %d.\n", rank, commSize);
+
     // double initialTheta[NUM_OPTIMIZATION_PARAMS] = {0};
     // getLocalOptimum(initialTheta);
 
     UserData udata = getMyUserData();
     getLocalOptimum(udata->am_p);
+
+    MPI_Finalize();
+
 }
 
 
