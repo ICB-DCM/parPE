163ff112c3a6fdb1b278109b01883ec42f9ed7ee
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2016-10-05 15:38:07 +0200

    some cleanup

--- main.c
+++ main.c
@@ -12,43 +12,58 @@
 #include "objectivefunction.h"
 #include "logger.h"
 
+// MPE event IDs for logging
 int mpe_event_begin_simulate, mpe_event_end_simulate;
 int mpe_event_begin_getrefs, mpe_event_end_getrefs;
 int mpe_event_begin_aggregate, mpe_event_end_aggregate;
 
 int main(int argc, char **argv)
 {
-    int commSize, rank, err;
+    int mpiCommSize, mpiRank, mpiErr;
 
-    err = MPI_Init(&argc, &argv);
-    err = MPI_Comm_size(MPI_COMM_WORLD, &commSize);
-    err = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    assert(commSize > 1);
+    mpiErr = MPI_Init(&argc, &argv);
+    mpiErr = MPI_Comm_size(MPI_COMM_WORLD, &mpiCommSize);
+    mpiErr = MPI_Comm_rank(MPI_COMM_WORLD, &mpiRank);
+    assert(mpiCommSize > 1);
 
-    err = MPE_Init_log();
+    int mpeErr = MPE_Init_log();
+    getMpeLogIDs();
 
-    MPE_Log_get_state_eventIDs(&mpe_event_begin_simulate, &mpe_event_end_simulate);
-    MPE_Log_get_state_eventIDs(&mpe_event_begin_aggregate, &mpe_event_end_aggregate);
-    MPE_Log_get_state_eventIDs(&mpe_event_begin_getrefs, &mpe_event_end_getrefs);
-
-    if(rank == 0) {
-        err = MPE_Describe_state(mpe_event_begin_simulate, mpe_event_end_simulate, "simulate", "blue:gray");
-        err = MPE_Describe_state(mpe_event_begin_aggregate, mpe_event_end_aggregate, "aggregate", "red:gray");
-        err = MPE_Describe_state(mpe_event_begin_getrefs, mpe_event_end_getrefs, "getrefs", "green:gray");
+    if(mpiRank == 0) {
+        describeMpeStates();
 
         // double initialTheta[NUM_OPTIMIZATION_PARAMS] = {0};
         // getLocalOptimum(initialTheta);
 
         loggerdata datalogger = initResultHDFFile("results.h5", "/results/multistarts/0");
+
         UserData udata = getMyUserData();
+
         getLocalOptimum(udata->am_p, &datalogger);
+
         freeUserData(udata);
+
         closeResultHDFFile(datalogger);
+
         sendTerminationSignalToAllWorkers();
+
     } else {
         doWorkerWork();
     }
 
-    err = MPE_Finish_log("mpe.log");
-    err = MPI_Finalize();
+    mpiErr = MPE_Finish_log("mpe.log");
+    mpiErr = MPI_Finalize();
+}
+
+void getMpeLogIDs() {
+    MPE_Log_get_state_eventIDs(&mpe_event_begin_simulate, &mpe_event_end_simulate);
+    MPE_Log_get_state_eventIDs(&mpe_event_begin_aggregate, &mpe_event_end_aggregate);
+    MPE_Log_get_state_eventIDs(&mpe_event_begin_getrefs, &mpe_event_end_getrefs);
+}
+
+void describeMpeStates() {
+    int mpeErr;
+    mpeErr = MPE_Describe_state(mpe_event_begin_simulate, mpe_event_end_simulate, "simulate", "blue:gray");
+    mpeErr = MPE_Describe_state(mpe_event_begin_aggregate, mpe_event_end_aggregate, "aggregate", "red:gray");
+    mpeErr = MPE_Describe_state(mpe_event_begin_getrefs, mpe_event_end_getrefs, "getrefs", "green:gray");
 }
