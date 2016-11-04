4c15179acbea1996c30a4c9f6ee287e62c3f0403
daniel.weindl <daniel.weindl@helmholtz-muenchen.de>
2016-11-04 13:45:20 +0100

    Add text colors

--- misc.h
+++ misc.h
@@ -1,7 +1,16 @@
 #ifndef CPP_MISC_H
 #define CPP_MISC_H
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 void error(const char *message);
+
 void warning(const char *message);
 
 #endif
