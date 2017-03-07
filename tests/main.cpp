8af4c71c8f299031d22d9f7e74c7b74f628724ce
daniel.weindl <daniel.weindl@helmholtz-muenchen.de>
2017-03-07 15:04:29 +0100

    Added tests for queue initialization, termination and queueing

--- tests/main.cpp
+++ tests/main.cpp
@@ -9,18 +9,26 @@ TEST_GROUP_C_WRAPPER(objectivefunction)
     TEST_GROUP_C_SETUP_WRAPPER(objectivefunction)
     TEST_GROUP_C_TEARDOWN_WRAPPER(objectivefunction)
 };
-
 TEST_C_WRAPPER(objectivefunction, test_reachedSteadyState)
-
 
 TEST_GROUP_C_WRAPPER(mpiworker)
 {
     TEST_GROUP_C_SETUP_WRAPPER(mpiworker)
     TEST_GROUP_C_TEARDOWN_WRAPPER(mpiworker)
 };
-
 TEST_C_WRAPPER(mpiworker, test_serializeResultPackageMessage)
 TEST_C_WRAPPER(mpiworker, test_serializeWorkPackageMessage)
+
+TEST_GROUP_C_WRAPPER(masterqueue)
+{
+    TEST_GROUP_C_SETUP_WRAPPER(masterqueue)
+    TEST_GROUP_C_TEARDOWN_WRAPPER(masterqueue)
+};
+TEST_C_WRAPPER(masterqueue, test_queueinit)
+TEST_C_WRAPPER(masterqueue, test_queue)
+TEST_C_WRAPPER(masterqueue, test_terminateMasterQueue_noInit)
+TEST_C_WRAPPER(masterqueue, test_queue_reinitialization)
+
 
 int main(int argc, char** argv)
 {
--- tests/testmasterqueue.c
+++ tests/testmasterqueue.c
@@ -1,6 +1,8 @@
 #include "CppUTest/TestHarness_c.h"
 #include "CppUTestExt/MockSupport_c.h"
 
+#include <unistd.h>
+
 // include .c because of static functions
 #include "masterqueue.c"
 
@@ -19,21 +21,68 @@ TEST_GROUP_C_SETUP(masterqueue) {
 }
 
 TEST_GROUP_C_TEARDOWN(masterqueue) {
+    mock_c()->checkExpectations();
+    mock_c()->clear();
 }
 
-// TODO: requires MPI_Init
-//TEST_C(masterqueue, test_queueinit) {
-//    initMasterQueue();
+// mock for MPI_Comm_size
+int MPI_Comm_size(MPI_Comm comm, int *size) {
+    mock_c()->actualCall("MPI_Comm_size");
+    *size = 10;
+    return 1; // ?
+}
 
-//    CHECK_EQUAL_C_BOOL(true, queueCreated);
-//    int actVal;
-//    sem_getvalue(&actValsemQueue, &actVal);
-//    CHECK_EQUAL_C_BOOL(true, actVal > 0);
-//}
+int MPI_Testany(int count, MPI_Request array_of_requests[], int *index,
+                               int *flag, MPI_Status *status) {
+    // mock_c()->actualCall("MPI_Testany");
 
-//TEST_C(masterqueue, test_queue) {
+    sleep(1000); // do nothing and wait to be killed
 
-//}
+    return 0;
+}
+
+TEST_C(masterqueue, test_queueinit) {
+    mock_c()->expectOneCall("MPI_Comm_size");
+    // Can happen or not, depending on how quick it's terminated
+    // mock_c()->expectOneCall("MPI_Testany");
+
+    initMasterQueue();
+
+    CHECK_EQUAL_C_BOOL(true, queueCreated);
+    int actVal;
+    sem_getvalue(&semQueue, &actVal);
+    CHECK_EQUAL_C_BOOL(true, actVal > 0);
+
+    terminateMasterQueue();
+}
+
+TEST_C(masterqueue, test_queue) {
+    mock_c()->expectNCalls(1, "MPI_Comm_size");
+    initMasterQueue();
+
+    queueData data;
+    queueSimulation(&data);
+    CHECK_EQUAL_C_BOOL(false, queueEnd == 0);
+    CHECK_EQUAL_C_BOOL(false, queueStart == 0);
+    CHECK_EQUAL_C_BOOL(true, queueEnd == queueStart);
+    CHECK_EQUAL_C_INT(1, lastJobId);
+
+    queueSimulation(&data);
+    CHECK_EQUAL_C_BOOL(false, queueEnd == queueStart);
+    CHECK_EQUAL_C_INT(2, lastJobId);
+    // getNextJob()
+
+    terminateMasterQueue();
+}
+
+TEST_C(masterqueue, test_queue_reinitialization) {
+    // should not crash or cause memory leaks
+    mock_c()->expectNCalls(1, "MPI_Comm_size");
+    initMasterQueue();
+    initMasterQueue();
+    terminateMasterQueue();
+}
+
 
 TEST_C(masterqueue, test_terminateMasterQueue_noInit) {
     // terminate uninitialized masterQueue should not fail
