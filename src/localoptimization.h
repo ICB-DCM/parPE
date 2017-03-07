98d6f64923c2d8bb51a4633e619cf36b35a0bddc
daniel.weindl <daniel.weindl@helmholtz-muenchen.de>
2017-03-07 16:26:38 +0100

    Some comments

--- src/localoptimization.h
+++ src/localoptimization.h
@@ -3,6 +3,11 @@
 
 #include "dataprovider.h"
 
+/**
+ * @brief getLocalOptimum
+ * @param dataPath
+ * @return Returns 0 on success.
+ */
 int getLocalOptimum(datapath dataPath);
 
 #endif
--- src/masterthread.h
+++ src/masterthread.h
@@ -6,7 +6,18 @@
 void startParameterEstimation();
 void startObjectiveFunctionGradientCheck();
 
+/**
+ * @brief newMultiStartOptimization
+ * @param multiStartIndexVP
+ * @return always returns 0
+ */
 void *newMultiStartOptimization(void *multiStartIndexVP);
+
+/**
+ * @brief newLocalOptimization
+ * @param idVP
+ * @return Pointer to int indicating status. 0: success, != 0: failure
+ */
 void *newLocalOptimization(void *idVP);
 
 #endif
