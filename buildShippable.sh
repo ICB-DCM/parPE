33eb7edb6dc437b482ff31c0d907dd34c7cc81f4
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-06-18 14:08:15 +0200

    Update paths

--- buildShippable.sh
+++ buildShippable.sh
@@ -6,7 +6,7 @@ CC=mpicc CXX=mpiCC cmake -DAMICI_DIR=`pwd`/../ThirdParty/AMICI-master \
       -DIPOPT_LIBRARIES=/usr/lib/libipopt.a \
       -DCERES_LIBRARIES=/usr/lib/libceres.a \
       -DCERES_INCLUDE_DIRS="/usr/include/" \
-      -DCPPUTEST_DIR=`pwd`/../ThirdParty/cpputest-3.8/ \
+      -DCPPUTEST_DIR=`pwd`/../ThirdParty/AMICI-master/ThirdParty/cpputest-3.8/ \
       -DMPI_INCLUDE_DIR=/usr/include/openmpi-x86_64/ \
       -DMPI_LIBRARY=/usr/lib64/openmpi/lib/libmpi_cxx.so \
       -DBLAS_INCLUDE_DIRS=`pwd`/../ThirdParty/CBLAS/include \
