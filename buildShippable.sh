3312e311fa00a77ccb28b28ec5e66b0700047105
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-06-18 15:29:46 +0200

    Eigen path

--- buildShippable.sh
+++ buildShippable.sh
@@ -5,7 +5,7 @@ CC=mpicc CXX=mpiCC cmake -DAMICI_DIR=`pwd`/../ThirdParty/AMICI-master \
       -DIPOPT_INCLUDE_DIRS=/usr/include/coin/ \
       -DIPOPT_LIBRARIES=/usr/lib/libipopt.a \
       -DCERES_LIBRARIES=/usr/lib/libceres.a \
-      -DCERES_INCLUDE_DIRS="/usr/include/" \
+      -DCERES_INCLUDE_DIRS="/usr/include/;/usr/include/eigen3" \
       -DCPPUTEST_DIR=`pwd`/../ThirdParty/AMICI-master/ThirdParty/cpputest-3.8/ \
       -DMPI_INCLUDE_DIR=/usr/include/openmpi-x86_64/ \
       -DMPI_LIBRARY=/usr/lib64/openmpi/lib/libmpi_cxx.so \
