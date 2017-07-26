3da2563f4688ccc2c9077aad5cd784e71f8faed5
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-07-26 07:27:24 +0200

    Add exception class

---
+++ common/src/parpeException.cpp
@@ -0,0 +1,15 @@
+#include "parpeException.h"
+
+ParPEException::ParPEException(const char *message) : message(message)
+{
+
+}
+
+ParPEException::ParPEException(const std::string &message) : message(message)
+{
+
+}
+
+const char *ParPEException::what() const noexcept {
+    return message.c_str();
+}
---
+++ common/src/parpeException.h
@@ -0,0 +1,22 @@
+#ifndef PARPEEXCEPTION_H
+#define PARPEEXCEPTION_H
+
+#include <exception>
+#include <string>
+
+class ParPEException : public std::exception
+{
+public:
+    ParPEException(const char *message);
+
+    ParPEException(const std::string &message);
+
+    virtual ~ParPEException() throw (){}
+
+    virtual const char* what() const noexcept;
+
+protected:
+    std::string message;
+};
+
+#endif // PARPEEXCEPTION_H
