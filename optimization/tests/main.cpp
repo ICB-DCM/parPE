2f92c5cae604619ea964c7dbf38ec8ebacc14eff
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-08-26 17:57:56 +0200

    cleanup cmake

--- optimization/tests/main.cpp
+++ optimization/tests/main.cpp
@@ -9,5 +9,7 @@
 int main(int argc, char **argv) {
     srand(time(NULL));
 
+    // MemoryLeakWarningPlugin::turnOffNewDeleteOverloads();
+
     return CommandLineTestRunner::RunAllTests(argc, argv);
 }
