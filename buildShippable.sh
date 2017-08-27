edbb2f7b7ecbe6719c6cbe3468a40db037a06791
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-08-27 14:26:04 +0200

    Update cpputest paths

--- buildShippable.sh
+++ buildShippable.sh
@@ -9,7 +9,7 @@ CC=mpicc CXX=mpiCC cmake \
       -DIPOPT_LIBRARIES=/usr/lib/libipopt.so \
       -DCERES_LIBRARIES="/usr/lib/libceres.so;/usr/lib/x86_64-linux-gnu/libglog.so;/usr/lib/x86_64-linux-gnu/libgflags.so" \
       -DCERES_INCLUDE_DIRS="/usr/include/;/usr/include/eigen3" \
-      -DCPPUTEST_DIR=`pwd`/../ThirdParty/AMICI-master/ThirdParty/cpputest-3.8/ \
+      -DCPPUTEST_DIR=`pwd`/../ThirdParty/AMICI-master/ThirdParty/cpputest-master/ \
       -DMPI_INCLUDE_DIR=/usr/include/openmpi-x86_64/ \
       -DMPI_LIBRARY=/usr/lib64/openmpi/lib/libmpi_cxx.so \
       -DBLAS_INCLUDE_DIRS="/usr/include/" \
