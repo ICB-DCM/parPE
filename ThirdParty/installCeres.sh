693f14066d2558d83f4c35b793f8ecbed5db5dcc
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-09-12 08:50:23 +0200

    Update CERES build options

--- ThirdParty/installCeres.sh
+++ ThirdParty/installCeres.sh
@@ -15,6 +15,8 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=`pwd`/install \
       -DBUILD_SHARED_LIBS=ON \
+      -DBUILD_TESTING=OFF \
+      -DGFLAGS=OFF \
       -DEIGEN_INCLUDE_DIR=`pwd`/../../eigen-eigen-67e894c6cd8f/ \
       -DMINIGLOG=ON \
       ..
