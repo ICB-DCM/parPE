84805425590e6a5fe4442371e077105255af3cf3
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-10-06 22:10:08 +0200

    add ThirdParty/installDeps.sh

---
+++ ThirdParty/installDeps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# build parPE dependencies (run downloadPackages.sh before)
+
+SCRIPT_PATH="`dirname \"$0\"`"
+SCRIPT_PATH="`( cd \"$SCRIPT_PATH\" && pwd )`"
+
+# exit on error
+set -e
+
+./installCBLAS.sh
+ 
+./installCpputest.sh
+ 
+./installCeres.sh
+ 
+./installIpopt.sh
