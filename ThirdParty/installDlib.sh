e70e563bd65521539446089c361a3e7ceb3a8b7e
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-11-04 11:33:32 +0100

    Avoid constant recompilation of dlib

--- ThirdParty/installDlib.sh
+++ ThirdParty/installDlib.sh
@@ -10,3 +10,15 @@ if [ ! -d dlib-19.7 ]; then
   fi
   tar -xjf $DLIB_ARCHIVE
 fi 
+
+cd dlib-19.7/
+mkdir -p build && cd build/
+ cmake -DCMAKE_INSTALL_PREFIX="`pwd`/install" \
+    -DDLIB_NO_GUI_SUPPORT=ON \
+    -DDLIB_GIF_SUPPORT=OFF \
+    -DDLIB_JPEG_SUPPORT=OFF \
+    -DDLIB_PNG_SUPPORT=OFF \
+    -DDLIB_LINK_WITH_SQLITE3=OFF \
+    ..
+ 
+make -j12 && make install
