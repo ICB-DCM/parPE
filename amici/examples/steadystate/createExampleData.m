31734db325886e4ba1a98b033350ab599d014a39
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2017-12-09 19:04:30 +0100

    Modify example data

--- amici/examples/steadystate/createExampleData.m
+++ amici/examples/steadystate/createExampleData.m
@@ -13,26 +13,25 @@ function [ output_args ] = createExampleData(  )
     
     %% model & data dimensions
     model = model_steadystate_syms;
+    numConditions = 12;
+    sigmaY = 0.1; % measurement standard deviation
     t = linspace(0,100,20); % timepoints
     t = [t, inf];
     p = [1;0.5;0.4;2;0.1]; % optimization parameters
     assert(numel(p) == numel(model.sym.p));
-    % k_original = [0.1,0.4,0.7,1]; % fixed parameters
+    k_original = [0.1,0.4,0.7,1]; % fixed parameters
     
     options = amioption('sensi', 0, ...
                         'maxsteps', 1e4, ...
                         'pscale', 'log10');
 
-    numConditions = 12;
     numK = numel(model.sym.k); % number of constant parameters
     numP = numel(model.sym.p); % number of variable parameters
     numY = numel(model.sym.y); % number of observables
     numT = numel(t); % number of timepoints
-    sigmaY = 0.1; % measurement standard deviation
     
     rng(0);
-    
-    kAll = ones(numConditions, numK);
+    kAll = repmat(k_original,numConditions,1);
     
     %% write data
     delete(hdfFile);
@@ -41,6 +40,9 @@ function [ output_args ] = createExampleData(  )
     end
     try
         h5create(hdfFile, '/measurements/ytrue', [numT, numY, numConditions]);
+    end
+    try
+        h5create(hdfFile, '/measurements/llhtrue', [numConditions]);
     end
     try
         h5create(hdfFile, '/measurements/y', [numT, numY, numConditions]);
@@ -63,14 +65,22 @@ function [ output_args ] = createExampleData(  )
         k = kAll(i, :);
         sol = simulate_model_steadystate(t, p, k, [], options);
         h5write(hdfFile, '/measurements/ytrue', sol.y, [1, 1, i], [numT, numY, 1]);
-        h5write(hdfFile, '/measurements/y', sol.y * (1 + sigmaY * randn()), [1, 1, i], [numT, numY, 1]);
+        h5write(hdfFile, '/measurements/y', sol.y, [1, 1, i], [numT, numY, 1]);
+        data = amidata(numT, numY, 0, 0, 0);
+        data.Y = sol.y;
+        sol = simulate_model_steadystate(t, p, k, data, options);
+        h5write(hdfFile, '/measurements/llhtrue', sol.llh, [i], [1]);
+        %h5write(hdfFile, '/measurements/y', sol.y * (1 + sigmaY * randn()), [1, 1, i], [numT, numY, 1]);
     end
+    
+    llh = h5read(hdfFile, '/measurements/llhtrue');
+    h5writeatt(hdfFile, '/measurements/llhtrue', 'total', sum(llh));
     
     %% set bounds
     h5create(hdfFile, '/parameters/lowerBound', [numP])
-    h5write(hdfFile, '/parameters/lowerBound', -2 * ones(numP, 1))
+    h5write(hdfFile, '/parameters/lowerBound', -4 * ones(numP, 1))
     h5create(hdfFile, '/parameters/upperBound', [numel(p)])
-    h5write(hdfFile, '/parameters/upperBound', 2 * ones(numP, 1))
+    h5write(hdfFile, '/parameters/upperBound', 4 * ones(numP, 1))
     
     %% set optimization options
     % create group
