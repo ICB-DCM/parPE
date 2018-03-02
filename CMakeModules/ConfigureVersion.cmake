cda3960b522dd54614481dd05e1be98f9c24b719
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-03-02 21:41:15 +0100

    Splitted version to header

--- CMakeModules/ConfigureVersion.cmake
+++ CMakeModules/ConfigureVersion.cmake
@@ -1,5 +1,8 @@
 # Version number from git
 set(PARPE_VERSION "unknown")
 include("${CMAKE_CURRENT_LIST_DIR}/getVersion.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/split_version.cmake")
+split_version(${PARPE_VERSION} libname major minor patch)
+#message(STATUS "${libname} ${major} ${minor} ${patch}  ")
 configure_file(${INFILE} ${OUTFILE} @ONLY)
 message(STATUS "Buildung version ${PARPE_VERSION}")
