c8d49506e476fa5dcc3be2226daf59d2feee56fd
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-03-02 21:34:17 +0100

    Clean up cmake version stuff. Add splitting function

--- CMakeModules/ConfigureVersion.cmake
+++ CMakeModules/ConfigureVersion.cmake
@@ -1,11 +1,5 @@
 # Version number from git
 set(PARPE_VERSION "unknown")
-find_package(Git)
-if(Git_FOUND)
-    execute_process(COMMAND sh -c "${GIT_EXECUTABLE} describe --abbrev=4 --dirty=-dirty --always --tags  | tr -d '\n'"
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-        OUTPUT_VARIABLE PARPE_VERSION
-        )
-endif()
+include("${CMAKE_CURRENT_LIST_DIR}/getVersion.cmake")
 configure_file(${INFILE} ${OUTFILE} @ONLY)
 message(STATUS "Buildung version ${PARPE_VERSION}")
