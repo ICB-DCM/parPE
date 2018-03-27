Fix: Write parameter scaling for analytically computed parameter correctly. Offset parameters were ignored before. Also, write parameter scaling for those parameters.

misc/generateHDF5DataFileFromText.py
@@ -24,6 +24,9 @@ import re
 import os
 from termcolor import colored
 
+SCALING_LOG10 = 2
+SCALING_LIN = 0
+
 class HDF5DataGenerator:
     """    
     Generate HDF5 file with fixed parameters and measurements for an AMICI-imported SBML model
@@ -510,6 +513,11 @@ class HDF5DataGenerator:
         dset.attrs['numOffsets'] = len(offsetsForHierarchicalIndices)
         dset[:] = use
         
+        dset = self.f.require_dataset("/offsetParameterScale", 
+                              shape=(len(offsetsForHierarchicalIndices),), 
+                              dtype='<i4', 
+                              data=np.full(len(offsetsForHierarchicalIndices), SCALING_LIN))
+        
     def ensureOffsetIsOffsetWithPositiveSign(self, scaling):
         """
         Current parPE implementation of hierarchical optimization assumes that offset parameters have positive sign.
@@ -578,7 +586,12 @@ class HDF5DataGenerator:
         dset.attrs['numScalings'] = len(scalingsForHierarchicalIndices)
         dset[:] = use
             
-                    
+        dset = self.f.require_dataset("/scalingParameterScale", 
+                      shape=(len(scalingsForHierarchical),), 
+                      dtype='<i4', 
+                      data=np.full(len(scalingsForHierarchical), SCALING_LIN))
+
+
     def ensureProportionalityFactorIsProportionalityFactor(self, scaling):
         """
         Ensure that this is a proportionality factor (a as in y = ax + b)
@@ -634,26 +647,29 @@ class HDF5DataGenerator:
         self.f.require_dataset('/amiciOptions/ts', shape=(len(self.timepoints),), dtype="f8", data=self.timepoints)
 
         # set pscale based on whether is scaling parameter (log10 for non-hierarchical, lin for hierarchical)
-        linParametersAmiciIndices = self.getAnalyticallyComputedParameterIndices()
+        linParametersAmiciIndices = self.getAnalyticallyComputedSimulationParameterIndices()
         self.f.require_dataset('/amiciOptions/pscale', shape=(np,), dtype="<i4", data=[2 * (ip not in linParametersAmiciIndices) for ip in range(np) ])
         # TODO for above parameters, the bounds must be adapted for non-hierarchical optimization!
         #  or use different pscale then
         
-    def getAnalyticallyComputedParameterIndices(self):
-        indices = []
+    def getAnalyticallyComputedSimulationParameterIndices(self):
+        """
+        Get model parameter index (not optimization parameter index) of all analytically computed parameters
+        """
+        parameterNamesModel = []
         if '/offsetParameterIndices' in self.f:
-            offsetNames = self.f['/parameters/parameterNames'][self.f['/offsetParameterIndices']]
-            indices = set([self.getGenericScalingParameterName(o) for o in offsetNames])
+            parameterNamesOptimization = self.f['/parameters/parameterNames'][self.f['/offsetParameterIndices']]
+            parameterNamesModel.extend(set([self.getGenericScalingParameterName(o) for o in parameterNamesOptimization]))
             
         if '/scalingParameterIndices' in self.f:
-            offsetNames = self.f['/parameters/parameterNames'][self.f['/scalingParameterIndices']]
-            indices = set([self.getGenericScalingParameterName(o) for o in offsetNames])
+            parameterNamesOptimization = self.f['/parameters/parameterNames'][self.f['/scalingParameterIndices']]
+            parameterNamesModel.extend(set([self.getGenericScalingParameterName(o) for o in parameterNamesOptimization]))
 
         if '/sigmaParameterIndices' in self.f:
-            offsetNames = self.f['/parameters/parameterNames'][self.f['/sigmaParameterIndices']]
-            indices = set([self.getGenericScalingParameterName(o) for o in offsetNames])
+            parameterNamesOptimization = self.f['/parameters/parameterNames'][self.f['/sigmaParameterIndices']]
+            parameterNamesModel.extend(set([self.getGenericScalingParameterName(o) for o in parameterNamesOptimization]))
 
-        return [ self.f['/parameters/modelParameterNames'][:].tolist().index(p) for p in indices ]
+        return [ self.f['/parameters/modelParameterNames'][:].tolist().index(p) for p in set(parameterNamesModel) ]
         
 
     def writeOptimizationOptions(self):