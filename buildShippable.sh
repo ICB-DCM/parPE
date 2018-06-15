71d99abda4acd4320d81a5dea1f0ae82f6a77dd7
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-06-15 19:52:10 +0200

    ci() set cpputest dir

--- buildShippable.sh
+++ buildShippable.sh
@@ -9,7 +9,7 @@ CC=mpicc CXX=mpiCC cmake \
       -DIPOPT_LIBRARIES=/usr/lib/libipopt.so \
       -DCERES_LIBRARIES="/usr/lib/libceres.so;/usr/lib/x86_64-linux-gnu/libglog.so;/usr/lib/x86_64-linux-gnu/libgflags.so" \
       -DCERES_INCLUDE_DIRS="/usr/include/;/usr/include/eigen3" \
-      -DCPPUTEST_DIR=`pwd`/../deps/AMICI/ThirdParty/cpputest-master/ \
+      -DCPPUTEST_DIR=`pwd`/../deps/AMICI/ThirdParty/cpputest-master/build/ \
       -DMPI_INCLUDE_DIR=/usr/include/openmpi-x86_64/ \
       -DMPI_LIBRARY=/usr/lib64/openmpi/lib/libmpi_cxx.so \
       -DGCOVR_REPORT=TRUE \
