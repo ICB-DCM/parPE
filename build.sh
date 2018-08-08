d1c2ca9e842491d32b951646e1099dc78e314d4b
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-08-08 12:31:47 +0200

    Only use pkg-config output on success

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ mkdir -p $PARPE_ROOT/build
 cd $PARPE_ROOT/build
 CC=mpicc CXX=mpiCC cmake \
       -DIPOPT_DIR=${PARPE_ROOT}/ThirdParty/Ipopt-3.12.9/install \
-      -DCERES_LIBRARIES="${PARPE_ROOT}/ThirdParty/ceres-solver-1.13.0/build/install/lib/libceres.a;`pkg-config --libs blas`;cxsparse;lapack;cholmod;camd;colamd" \
+      -DCERES_LIBRARIES="${PARPE_ROOT}/ThirdParty/ceres-solver-1.13.0/build/install/lib/libceres.a;`pkg-config --libs blas 2> /dev/null`;cxsparse;lapack;cholmod;camd;colamd" \
       -DCERES_INCLUDE_DIRS="${PARPE_ROOT}/ThirdParty/ceres-solver-1.13.0/build/install/include/;${PARPE_ROOT}/ThirdParty/ceres-solver-1.13.0/build/install/include/ceres/internal/miniglog/;${PARPE_ROOT}/ThirdParty/eigen-eigen-67e894c6cd8f/build/install/include/eigen3/" \
       -DMPI_INCLUDE_DIR=/usr/include/openmpi-x86_64/ \
       -DMPI_LIBRARY=/usr/lib64/openmpi/lib/libmpi_cxx.so \
