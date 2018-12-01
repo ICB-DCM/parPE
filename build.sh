04f9e6b2a7389240da1a153bb4d3091780ee5fc8
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-12-01 22:47:06 +0100

    Cleanup build scripts

--- build.sh
+++ build.sh
@@ -15,13 +15,13 @@ AMICI_DIR=$PARPE_ROOT/deps/AMICI/
 # build parpe
 mkdir -p $PARPE_ROOT/build
 cd $PARPE_ROOT/build
+
 CC=mpicc CXX=mpiCC cmake \
       -DIPOPT_DIR=${PARPE_ROOT}/ThirdParty/Ipopt-3.12.9/install \
       -DCERES_LIBRARIES="${PARPE_ROOT}/ThirdParty/ceres-solver-1.13.0/build/install/lib/libceres.a;`pkg-config --libs blas 2> /dev/null`;cxsparse;lapack;cholmod;camd;colamd" \
       -DCERES_INCLUDE_DIRS="${PARPE_ROOT}/ThirdParty/ceres-solver-1.13.0/build/install/include/;${PARPE_ROOT}/ThirdParty/ceres-solver-1.13.0/build/install/include/ceres/internal/miniglog/;${PARPE_ROOT}/ThirdParty/eigen-eigen-67e894c6cd8f/build/install/include/eigen3/" \
-      -DMPI_INCLUDE_DIR=/usr/include/openmpi-x86_64/ \
-      -DMPI_LIBRARY=/usr/lib64/openmpi/lib/libmpi_cxx.so \
-      -DCppUTest_DIR=${PARPE_ROOT}/deps/AMICI/ThirdParty/cpputest-master/build-noleakcheck/ \
+      -DMPI_INCLUDE_DIRS=/usr/include/openmpi-x86_64/ \
+      -DCppUTest_ROOT=${PARPE_ROOT}/deps/AMICI/ThirdParty/cpputest-master/build-noleakcheck/ \
       -DENABLE_SWIG=FALSE \
       $PARPE_ROOT
 make -j12
--- buildShippable.sh
+++ buildShippable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build script for CI on shippable.com
 
-mkdir build
+mkdir -p build
 cd build
 
 CC=mpicc CXX=mpiCC cmake \
@@ -9,9 +9,8 @@ CC=mpicc CXX=mpiCC cmake \
       -DIPOPT_LIBRARIES=/usr/lib/libipopt.so \
       -DCERES_LIBRARIES="/usr/lib/libceres.so;/usr/lib/x86_64-linux-gnu/libglog.so;/usr/lib/x86_64-linux-gnu/libgflags.so" \
       -DCERES_INCLUDE_DIRS="/usr/include/;/usr/include/eigen3" \
-      -DCppUTest_DIR=`pwd`/../deps/AMICI/ThirdParty/cpputest-master/build/ \
-      -DMPI_INCLUDE_DIR=/usr/include/openmpi-x86_64/ \
-      -DMPI_LIBRARY=/usr/lib64/openmpi/lib/libmpi_cxx.so \
+      -DCppUTest_ROOT=`pwd`/../deps/AMICI/ThirdParty/cpputest-master/build/ \
+      -DMPI_INCLUDE_DIRS=/usr/include/openmpi-x86_64/ \
       -DGCOVR_REPORT=TRUE \
       ..
 
