8cc5450028c7c6709b9d355f67ca84e87323ec6a
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-02-13 22:36:27 +0100

    Add scripts for benchmark collection model import and test

---
+++ benchmark_collection/all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Import, build, and run all benchmark models
+
+BENCHMARK_DIR=${HOME}/src/Benchmark-Models-Caro/hackathon_contributions_new_data_format/
+
+for MODEL in $(ls -1d ${BENCHMARK_DIR}/*/); do
+    printf '=%.0s' {1..20}
+    printf ${MODEL}
+    printf '=%.0s' {1..20}
+    echo
+    ./import_and_run.sh ${MODEL}
+    printf '=%.0s' {1..100}
+    echo
+done
