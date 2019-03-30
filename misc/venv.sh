45299da01d7d87171863be426a5fa70a4510de62
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-03-30 09:36:53 +0100

    Cleanup venv creation. Skip if directory exists to speedup building.

--- misc/venv.sh
+++ misc/venv.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 #
-# Setup virtual environment
+# Setup virtual environment for building/testing parPE
 #
 set -e
 
 SCRIPT_PATH=$(dirname $BASH_SOURCE)
-PARPE_ROOT=$(cd $SCRIPT_PATH/.. && pwd)
+PARPE_ROOT=$(cd ${SCRIPT_PATH}/.. && pwd)
 BUILD_DIR=$1
 
+# set default build dir if not provided
+if [[ -z "${BUILD_DIR}" ]]; then BUILD_DIR="${PARPE_ROOT}/build"; fi
+
 # Save the time for installing AMICI if the venv already exists
-# NOTE: Remove folder if AMICI is updated
-if [ ! -d ${BUILD_DIR}/venv ]; then
+# NOTE: Must remove folder if AMICI is updated
+if [[ ! -d ${BUILD_DIR}/venv ]]; then
     # create venv
     python3 -m venv ${BUILD_DIR}/venv
     source ${BUILD_DIR}/venv/bin/activate
 
-    # install amici
+    # install AMICI
     cd ${PARPE_ROOT}/deps/AMICI/python/sdist
     pip3 install -e .
 
-    deactivate
+    # install parPE
+    cd ${PARPE_ROOT}/python
+    pip3 install -e .
 fi
-
-source ${BUILD_DIR}/venv/bin/activate
-
-# install misc
-pip3 install -U termcolor colorama
-
-# install parpe
-cd ${PARPE_ROOT}/python
-pip3 install -e .
