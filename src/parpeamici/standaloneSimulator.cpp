c0d9545b2ea3e7362f7648fd77f65619a794f8d3
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-05-28 10:23:39 +0200

    Ensure hdf5 mutex lock when using AMICI hdf5 helper functions (Closes #167)

--- src/parpeamici/standaloneSimulator.cpp
+++ src/parpeamici/standaloneSimulator.cpp
@@ -103,6 +103,7 @@ int StandaloneSimulator::run(const std::string& resultFile,
 
         auto resultFileH5 = rw.reopenFile();
         hdf5EnsureGroupExists(resultFileH5.getId(), resultPath.c_str());
+        auto lock = hdf5MutexGetLock();
         amici::hdf5::createAndWriteDouble1DDataset(
                     resultFileH5, resultPath + "/parameters", parameters);
     }
@@ -184,9 +185,11 @@ int StandaloneSimulator::run(const std::string& resultFile,
                     scalings, offsets, sigmas);
         auto resultFileH5 = rw.reopenFile();
         hdf5EnsureGroupExists(resultFileH5.getId(), resultPath.c_str());
-        amici::hdf5::createAndWriteDouble1DDataset(
-                    resultFileH5, resultPath + "/parameters", parameters);
-
+        {
+            auto lock = hdf5MutexGetLock();
+            amici::hdf5::createAndWriteDouble1DDataset(
+                        resultFileH5, resultPath + "/parameters", parameters);
+        }
         // compute llh
         for(int conditionIdx = 0; (unsigned) conditionIdx < simulationResults.size(); ++conditionIdx) {
             double llh = -parpe::computeNegLogLikelihood(
