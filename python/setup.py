90f94adb04edc6f65143c6c44219ffe5411535d8
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-08-26 15:36:06 +0200

    Update AMICI requirement

--- python/setup.py
+++ python/setup.py
@@ -12,7 +12,7 @@ setup(
                       'termcolor',
                       'colorama',
                       'petab>=0.0.0a14',
-                      'amici>=0.10.7',
+                      'amici>=0.10.10',
                       'h5py',
                       'python-libsbml>=5.17.0',
                       'jinja2'
