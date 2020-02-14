f0e2c2317a86b35e5095cc258878bae107ce36be
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-02-14 14:00:45 +0100

    ..

--- benchmark_collection/all.sh
+++ benchmark_collection/all.sh
@@ -13,9 +13,8 @@ Borghans_BiophysChem1997
 Elowitz_Nature2000
 Chen_MSB2009
 Fujita_SciSignal2010
-Sneyd_PNAS2002
-Zheng_PNAS2012"
-# Schwen_PONE2014 TODO: shadowing observables
+Sneyd_PNAS2002"
+# Schwen_PONE2014 Zheng_PNAS2012 TODO: shadowing observables
 
 
 for MODEL in $expected_to_work; do
