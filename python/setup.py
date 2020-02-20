1c54397eb8b6a1bfbd9be75b171891a39a294716
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-02-20 13:45:15 +0100

    Require new PEtab

--- python/setup.py
+++ python/setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=['numpy',
                       'termcolor',
                       'colorama',
-                      'petab>=0.1.1',
+                      'petab>=0.1.2',
                       'amici>=0.10.18',
                       'h5py',
                       'python-libsbml>=5.17.0',
