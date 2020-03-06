8eccda0baf023f3446b7a391f598340446dfb9b0
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-03-06 15:30:31 +0100

    petab>=0.1.4

--- python/setup.py
+++ python/setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=['numpy',
                       'termcolor',
                       'colorama',
-                      'petab>=0.1.3',
+                      'petab>=0.1.4',
                       'amici>=0.10.19',
                       'h5py',
                       'python-libsbml>=5.17.0',
