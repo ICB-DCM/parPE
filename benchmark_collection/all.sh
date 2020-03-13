eb67d9ca36ad75d9f92d95d0f53f5f0513cd6d78
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-03-13 21:10:48 +0100

    Update to new benchmark collection repo

--- benchmark_collection/all.sh
+++ benchmark_collection/all.sh
@@ -14,9 +14,8 @@ Elowitz_Nature2000
 Chen_MSB2009
 Fujita_SciSignal2010
 Sneyd_PNAS2002
-Schwen_PONE2014
 Zheng_PNAS2012"
-
+#Schwen_PONE2014
 
 for MODEL in $expected_to_work; do
   printf '=%.0s' {1..20}
