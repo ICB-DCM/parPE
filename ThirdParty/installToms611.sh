5ee393da348ad2de7d1f5996a259a26c87cf8d49
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-04-02 20:22:06 +0200

    Fix: Handle paths with blanks in build scripts (Fixes #246)

--- ThirdParty/installToms611.sh
+++ ThirdParty/installToms611.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
-set -e 
-
 # build f2c
 
-F2C_ARCHIVE=libf2c.zip
+set -euo pipefail
+
+f2c_archive=libf2c.zip
 if [ ! -d libf2c ]; then
-  if [ ! -f $F2C_ARCHIVE ]; then
-    wget "http://www.netlib.org/f2c/libf2c.zip" -o $DLIB_ARCHIVE
+  if [ ! -f "${f2c_archive}" ]; then
+    wget "http://www.netlib.org/f2c/libf2c.zip" -o "${f2c_archive}"
   fi
-    unzip $F2C_ARCHIVE -d libf2c
-fi 
+    unzip "${f2c_archive}" -d libf2c
+fi
 
 cd libf2c
 cp makefile.u Makefile
@@ -19,4 +18,4 @@ make hadd
 make -j12
 
 cd ../toms611/
-ln -sf ../libf2c/f2c.h
+ln -sf ../libf2c/f2c.h .
