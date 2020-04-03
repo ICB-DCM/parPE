82e11763f78096db4d603a44551f71b630ce80aa
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-04-03 22:59:40 +0200

    MEMORYCHECK_COMMAND_OPTIONS is now MemoryCheckCommandOptions

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,9 +1,9 @@
 set (CTEST_PROJECT_NAME "parPE")
-set(MEMORYCHECK_COMMAND_OPTIONS
-    "${MEMORYCHECK_COMMAND_OPTIONS}
+set(MemoryCheckCommandOptions
+    "${MemoryCheckCommandOptions}
+        -v
         --leak-check=full
         --show-leak-kinds=all
         --error-exitcode=1
         --gen-suppressions=all
-        -v
         --suppressions=${CMAKE_CURRENT_LIST_DIR}/tests/parpe.supp")
