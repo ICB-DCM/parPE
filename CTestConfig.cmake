01a775c4365bb9ef25214c6bbd2011a3fbc02adf
Daniel Weindl <dweindl@users.noreply.github.com>
2020-04-04 01:18:23 +0200

    Use Github actions instead of shippable (#256)
    ... as shippable.com only allows for one single run at a time.
    
    * Separate benchmark model tests
    * Separate petab tests
    * Remove shippable.yml
    * coverage to sonar
    * Update valgrind suppressions

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,2 +1,9 @@
 set (CTEST_PROJECT_NAME "parPE")
-set(MEMORYCHECK_COMMAND_OPTIONS "${MEMORYCHECK_COMMAND_OPTIONS} --leak-check=full --error-exitcode=1 --gen-suppressions=all -v --suppressions=${CMAKE_CURRENT_LIST_DIR}/tests/parpe.supp")
+set(MEMORYCHECK_COMMAND_OPTIONS
+    "${MEMORYCHECK_COMMAND_OPTIONS} \
+        -v \
+        --leak-check=full \
+        --show-leak-kinds=all \
+        --error-exitcode=1 \
+        --gen-suppressions=all \
+        --suppressions=${CMAKE_CURRENT_LIST_DIR}/tests/parpe.supp")
