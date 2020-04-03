e88d112352e100cb8c7ad6a2d9259cfcdb641195
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-04-03 13:24:15 +0200

    Test sonarcloud

--- buildShippable.sh
+++ buildShippable.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 set -x
 
 # build parPE
-mkdir -p build
-cd build
+build_dir="$(pwd)/build"
 CC=mpicc CXX=mpiCC cmake \
+      -B"${build_dir}" \
       -DIPOPT_INCLUDE_DIRS=/usr/include/coin/ \
       -DIPOPT_LIBRARIES=/usr/lib/libipopt.so \
       -DCERES_LIBRARIES="/usr/lib/libceres.so;/usr/lib/x86_64-linux-gnu/libglog.so;/usr/lib/x86_64-linux-gnu/libgflags.so" \
@@ -16,6 +16,12 @@ CC=mpicc CXX=mpiCC cmake \
       -DBUILD_TESTS=TRUE \
       ..
 
-make -j12 VERBOSE=1
+build-wrapper-linux-x86-64 --out-dir bw-output cmake --build "${build_dir}" -j12 -- VERBOSE=1
 
-cd ..
+sonar-scanner \
+  -Dsonar.organization=icb-dcm \
+  -Dsonar.projectKey=ICB-DCM_parPE \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.cfamily.build-wrapper-output=bw-output \
+  -Dsonar.login=45879b85332d963bf3ead20399bf5bd2c925c156
