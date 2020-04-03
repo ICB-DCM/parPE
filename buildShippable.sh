bf104ebbd12166fef83418c442f41f9b87699050
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-04-03 13:42:35 +0200

    ..

--- buildShippable.sh
+++ buildShippable.sh
@@ -3,29 +3,35 @@
 set -euo pipefail
 set -x
 
-# build parPE
+# configure parPE
 src_dir="$(pwd)"
 build_dir="$(pwd)/build"
-CC=mpicc CXX=mpiCC cmake \
-      -S"${src_dir}" \
-      -B"${build_dir}" \
-      -DIPOPT_INCLUDE_DIRS=/usr/include/coin/ \
-      -DIPOPT_LIBRARIES=/usr/lib/libipopt.so \
-      -DCERES_LIBRARIES="/usr/lib/libceres.so;/usr/lib/x86_64-linux-gnu/libglog.so;/usr/lib/x86_64-linux-gnu/libgflags.so" \
-      -DCERES_INCLUDE_DIRS="/usr/include/;/usr/include/eigen3" \
-      -DMPI_INCLUDE_DIRS=/usr/include/openmpi-x86_64/ \
-      -DGCOVR_REPORT=TRUE \
-      -DBUILD_TESTS=TRUE \
-      ..
 
+CC=mpicc CXX=mpiCC cmake \
+  -S"${src_dir}" \
+  -B"${build_dir}" \
+  -DIPOPT_INCLUDE_DIRS=/usr/include/coin/ \
+  -DIPOPT_LIBRARIES=/usr/lib/libipopt.so \
+  -DCERES_LIBRARIES="/usr/lib/libceres.so;/usr/lib/x86_64-linux-gnu/libglog.so;/usr/lib/x86_64-linux-gnu/libgflags.so" \
+  -DCERES_INCLUDE_DIRS="/usr/include/;/usr/include/eigen3" \
+  -DMPI_INCLUDE_DIRS=/usr/include/openmpi-x86_64/ \
+  -DGCOVR_REPORT=TRUE \
+  -DBUILD_TESTS=TRUE \
+  ..
+
+# setup sonar cloud
 export SONAR_SCANNER_VERSION=4.2.0.1873
 export SONAR_SCANNER_HOME=/root/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
 export PATH=$SONAR_SCANNER_HOME/bin:$PATH
 export SONAR_SCANNER_OPTS="-server"
 export PATH=/root/.sonar/build-wrapper-linux-x86:$PATH
 
-build-wrapper-linux-x86-64 --out-dir bw-output cmake --build "${build_dir}" -j12 -- VERBOSE=1
+# build parpe
+build-wrapper-linux-x86-64 --out-dir bw-output \
+  cmake --build "${build_dir}" --parallel \
+   -- VERBOSE=1
 
+# submit to sonarcloud
 sonar-scanner \
   -Dsonar.organization=icb-dcm \
   -Dsonar.projectKey=ICB-DCM_parPE \
