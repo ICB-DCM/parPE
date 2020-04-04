0b0cc1caa59bdfa6869ed442e29037389a7fc2d9
Daniel Weindl <dweindl@users.noreply.github.com>
2020-04-04 20:39:44 +0200

    Update to AMICI v0.10.21 and petab 0.1.7 (#261)
    * git subrepo clone (merge) --branch=v0.10.21 --force git@github.com:ICB-DCM/AMICI.git deps/AMICI
    
    subrepo:
      subdir:   "deps/AMICI"
      merged:   "390305f9"
    upstream:
      origin:   "git@github.com:ICB-DCM/AMICI.git"
      branch:   "v0.10.21"
      commit:   "390305f9"
    git-subrepo:
      version:  "0.4.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a04d8c2"
    
    * Bump version requirements

--- python/setup.py
+++ python/setup.py
@@ -14,15 +14,15 @@ setup(
     author_email='daniel.weindl@helmholtz-muenchen.de',
     description='parpe python package',
     packages=find_packages(),
-    install_requires=['numpy',
-                      'termcolor',
-                      'colorama',
-                      'petab>=0.1.5',
-                      'amici>=0.10.20',
-                      'h5py',
+    install_requires=['numpy>=1.18.1',
+                      'termcolor>=1.1.0',
+                      'colorama>=0.4.3',
+                      'petab>=0.1.7',
+                      'amici>=0.10.21',
+                      'h5py>=2.10.0',
                       'python-libsbml>=5.17.0',
-                      'jinja2',
-                      'snakemake'
+                      'jinja2>=2.11.1',
+                      'snakemake>=5.10.0'
                       ],
     entry_points=ENTRY_POINTS,
 )
