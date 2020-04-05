bd57d72618698319701d855c7391b9033fc27b97
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-04-05 17:38:14 +0200

    Extract hierarchicalOptimizationAnalyticalParameterProvider from hierarchicalOptimization

---
+++ include/parpeamici/hierarchicalOptimizationAnalyticalParameterProvider.h
@@ -0,0 +1,160 @@
+#ifndef HIERARCHICALOPTIMIZATIONANALYTICALPARAMETERPROVIDER_H
+#define HIERARCHICALOPTIMIZATIONANALYTICALPARAMETERPROVIDER_H
+
+#include <vector>
+#include <map>
+
+#include <H5Cpp.h>
+
+namespace parpe {
+
+
+/**
+ * @brief The AnalyticalParameterProvider class is an interface for providing
+ * information on optimization parameters to be computed analytically
+ * (proportionality factors, offsets, sigmas, ...).
+ */
+class AnalyticalParameterProvider
+{
+  public:
+    virtual ~AnalyticalParameterProvider() {}
+
+    /**
+     * @brief Get vector of condition indices for which the parameter with the
+     * given index is used.
+     * @param parameterIndex referring to the index in the analytical parameter
+     * list in the hdf5 file
+     * (*not* the optimization parameter index).
+     * @return Vector of condition indice
+     */
+    virtual std::vector<int> getConditionsForParameter(
+        int parameterIndex) const = 0;
+
+    /**
+     * @brief Get vector of observable indices for the specified condition for
+     * which the specified parameter is used.
+     * @param parameterIndex
+     * @return
+     */
+    virtual std::vector<int> const& getObservablesForParameter(
+        int parameterIndex,
+        int conditionIdx) const = 0;
+
+    /**
+     * @brief Vector with indices of the of the analytically determined
+     * parameters within the overall optimization parameter vector
+     * @return
+     */
+    virtual std::vector<int> getOptimizationParameterIndices() const = 0;
+};
+
+class AnalyticalParameterProviderDefault : public AnalyticalParameterProvider
+{
+  public:
+    AnalyticalParameterProviderDefault() = default;
+
+    std::vector<int> getConditionsForParameter(
+        int parameterIndex) const override;
+
+    std::vector<int> const& getObservablesForParameter(
+        int parameterIndex,
+        int conditionIdx) const override;
+
+    std::vector<int> getOptimizationParameterIndices() const override;
+
+    // TODO private
+    std::vector<std::vector<int>> conditionsForParameter;
+    std::vector<int> optimizationParameterIndices;
+    // x[scalingIdx][conditionIdx] -> std::vector of observableIndicies
+    std::vector<std::map<int, std::vector<int>>> mapping;
+};
+
+/**
+ * @brief The AnalyticalParameterHdf5Reader class reads from an HDF5 file the
+ * dependencies of experimental conditions and observables on parameters which
+ * are to be computed analytically.
+ *
+ */
+class AnalyticalParameterHdf5Reader : public AnalyticalParameterProvider
+{
+  public:
+    AnalyticalParameterHdf5Reader() = default;
+
+    /**
+     * @brief AnalyticalParameterHdf5Reader
+     * @param file
+     * @param scalingParameterIndicesPath location in hdf5 file of the list of
+     * indices of the analytically determined parameters within the overall
+     * optimization parameters
+     * @param mapPath path of to the dataset with the
+     * parameter-oberservable-condition mapping
+     */
+    AnalyticalParameterHdf5Reader(const H5::H5File& file,
+                                  std::string analyticalParameterIndicesPath,
+                                  std::string mapPath);
+
+    AnalyticalParameterHdf5Reader(AnalyticalParameterHdf5Reader const&) =
+        delete;
+
+    /**
+     * @brief Get vector of condition indices for which the parameter with the
+     * given index is used.
+     * @param parameterIndex referring to the index in the analytical parameter
+     * list in the hdf5 file
+     * (*not* the optimization parameter index).
+     * @return Vector of condition indice
+     */
+    std::vector<int> getConditionsForParameter(
+        int parameterIndex) const override;
+
+    /**
+     * @brief Get vector of observable indices for the specified condition for
+     * which the specified parameter is used.
+     * @param parameterIndex
+     * @return
+     */
+    std::vector<int> const& getObservablesForParameter(
+        int parameterIndex,
+        int conditionIdx) const override;
+
+    /**
+     * @brief Vector with indices of the of the analytically determined
+     * parameters within the overall optimization parameter vector
+     * @return
+     */
+    std::vector<int> getOptimizationParameterIndices() const override;
+
+  private:
+    /**
+     * @brief Get number of analytically computed parameters
+     * @param dataset Read information from this dataset.
+     * @return
+     */
+    int getNumAnalyticalParameters() const;
+
+    /**
+     * @brief Read mapping of parameter to model outputs.
+     *
+     * Data is expected to come as matrix/table with the following columns:
+     * - scalingParameterIndex: 0-based index referring to entries in the
+     *     parameter index list
+     * - conditionIdx: condition index
+     * - observableIdx: index of model output
+     */
+    void readParameterConditionObservableMappingFromFile();
+    std::vector<int> readRawMap(H5::DataSet& dataset,
+                                hsize_t& nRows,
+                                hsize_t& nCols);
+
+    H5::H5File file;
+    std::string rootPath;
+    std::string mapPath;
+    std::string analyticalParameterIndicesPath;
+
+    // x[scalingIdx][conditionIdx] -> std::vector of observableIndicies
+    std::vector<std::map<int, std::vector<int>>> mapping;
+};
+
+
+}
+#endif // HIERARCHICALOPTIMIZATIONANALYTICALPARAMETERPROVIDER_H
---
+++ src/parpeamici/hierarchicalOptimizationAnalyticalParameterProvider.cpp
@@ -0,0 +1,163 @@
+#include <parpeamici/hierarchicalOptimizationAnalyticalParameterProvider.h>
+
+#include <parpecommon/hdf5Misc.h>
+#include <parpecommon/parpeException.h>
+
+namespace parpe {
+
+
+std::vector<int>
+AnalyticalParameterProviderDefault::getConditionsForParameter(
+    int parameterIndex) const
+{
+    return conditionsForParameter[parameterIndex];
+}
+
+const std::vector<int>&
+AnalyticalParameterProviderDefault::getObservablesForParameter(
+    int parameterIndex,
+    int conditionIdx) const
+{
+    return mapping[parameterIndex].at(conditionIdx);
+}
+
+std::vector<int>
+AnalyticalParameterProviderDefault::getOptimizationParameterIndices() const
+{
+    return optimizationParameterIndices;
+}
+
+AnalyticalParameterHdf5Reader::AnalyticalParameterHdf5Reader(
+    H5::H5File const& file,
+    std::string analyticalParameterIndicesPath,
+    std::string mapPath)
+    : mapPath(std::move(mapPath))
+      , analyticalParameterIndicesPath(std::move(analyticalParameterIndicesPath))
+{
+    auto lock = hdf5MutexGetLock();
+    this->file = file; // copy while mutex is locked!
+    readParameterConditionObservableMappingFromFile();
+}
+
+
+std::vector<int>
+AnalyticalParameterHdf5Reader::getConditionsForParameter(
+    int parameterIndex) const
+{
+    std::vector<int> result;
+    result.reserve(mapping[parameterIndex].size());
+    for (auto const& kvp : mapping[parameterIndex])
+        result.push_back(kvp.first);
+    return result;
+}
+
+const std::vector<int>&
+AnalyticalParameterHdf5Reader::getObservablesForParameter(
+    int parameterIndex,
+    int conditionIdx) const
+{
+    return mapping[parameterIndex].at(conditionIdx);
+}
+
+std::vector<int>
+AnalyticalParameterHdf5Reader::getOptimizationParameterIndices() const
+{
+    auto lock = hdf5MutexGetLock();
+    std::vector<int> analyticalParameterIndices;
+
+    if (file.nameExists(analyticalParameterIndicesPath)) {
+        auto dataset = file.openDataSet(analyticalParameterIndicesPath);
+        auto dataspace = dataset.getSpace();
+
+        auto ndims = dataspace.getSimpleExtentNdims();
+        if (ndims != 1)
+            throw ParPEException(
+                "Invalid dimension in getOptimizationParameterIndices.");
+        hsize_t numScalings = 0;
+        dataspace.getSimpleExtentDims(&numScalings);
+
+        analyticalParameterIndices.resize(numScalings);
+        dataset.read(analyticalParameterIndices.data(),
+                     H5::PredType::NATIVE_INT);
+    }
+    return analyticalParameterIndices;
+}
+
+int
+AnalyticalParameterHdf5Reader::getNumAnalyticalParameters() const
+{
+    hsize_t numAnalyticalParameters = 0;
+    auto lock = hdf5MutexGetLock();
+
+    if (file.nameExists(analyticalParameterIndicesPath)) {
+        auto dataset = file.openDataSet(analyticalParameterIndicesPath);
+        auto dataspace = dataset.getSpace();
+        auto ndims = dataspace.getSimpleExtentNdims();
+        if (ndims != 1)
+            throw ParPEException(
+                "Invalid dimension in getOptimizationParameterIndices.");
+        dataspace.getSimpleExtentDims(&numAnalyticalParameters);
+    }
+
+    return numAnalyticalParameters;
+}
+
+void
+AnalyticalParameterHdf5Reader::readParameterConditionObservableMappingFromFile()
+{
+    auto lock = hdf5MutexGetLock();
+    H5_SAVE_ERROR_HANDLER;
+    try {
+        int numScalings = getNumAnalyticalParameters();
+        auto dataset = file.openDataSet(mapPath);
+        if (numScalings == 0)
+            return;
+
+        // column indices in dataspace
+        constexpr int parameterCol = 0;
+        constexpr int conditionCol = 1;
+        constexpr int observableCol = 2;
+
+        mapping.resize(numScalings);
+
+        hsize_t nRows = 0, nCols = 0;
+        auto rawMap = readRawMap(dataset, nRows, nCols);
+
+        for (int i = 0; (unsigned)i < nRows; ++i) {
+            int scalingIdx = rawMap[i * nCols + parameterCol];
+            int conditionIdx = rawMap[i * nCols + conditionCol];
+            int observableIdx = rawMap[i * nCols + observableCol];
+            mapping[scalingIdx][conditionIdx].push_back(observableIdx);
+        }
+    } catch (H5::FileIException&) {
+        return;
+    }
+    H5_RESTORE_ERROR_HANDLER;
+}
+
+std::vector<int>
+AnalyticalParameterHdf5Reader::readRawMap(H5::DataSet& dataset,
+                                          hsize_t& nRows,
+                                          hsize_t& nCols)
+{
+    auto dataspace = dataset.getSpace();
+    auto ndims = dataspace.getSimpleExtentNdims();
+    if (ndims != 2)
+        throw ParPEException(
+            "Invalid dimension for analytical parameter map, expected 2.");
+
+    hsize_t dims[ndims];
+    dataspace.getSimpleExtentDims(dims);
+    nRows = dims[0];
+    nCols = dims[1];
+    if (nRows && nCols != 3)
+        throw ParPEException("Invalid dimension for analytical parameter map, "
+                             "expected 3 columns.");
+
+    std::vector<int> rawMap(nRows * nCols);
+    dataset.read(rawMap.data(), H5::PredType::NATIVE_INT);
+
+    return rawMap;
+}
+
+}
