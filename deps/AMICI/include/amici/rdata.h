6a541024cf21f8de20c690b052cd3054bdf9c1ad
Paul Stapor <paul.stapor@helmholtz-muenchen.de>
2020-04-28 11:04:13 +0200

    undo last commit

--- deps/AMICI/include/amici/rdata.h
+++ deps/AMICI/include/amici/rdata.h
@@ -388,18 +388,6 @@ class ReturnData {
     RDataReporting rdata_reporting{RDataReporting::full};
 
     /**
-     * @brief Set likelihood and chi2 to NaN
-     * (typically after integration failure)
-     */
-    void invalidateLLH();
-
-    /**
-     * @brief Set likelihood sensitivities to NaN
-     * (typically after integration failure)
-     */
-    void invalidateSLLH();
-
-    /**
      * @brief Serialize ReturnData (see boost::serialization::serialize)
      * @param ar Archive to serialize to
      * @param r Data to serialize
@@ -571,6 +559,18 @@ class ReturnData {
      * @param it_start time index at which to start invalidating
      */
     void invalidate(int it_start);
+
+    /**
+     * @brief Set likelihood and chi2 to NaN
+     * (typically after integration failure)
+     */
+    void invalidateLLH();
+
+    /**
+     * @brief Set likelihood sensitivities to NaN
+     * (typically after integration failure)
+     */
+    void invalidateSLLH();
 
     /**
      * @brief applies the chain rule to account for parameter transformation in
