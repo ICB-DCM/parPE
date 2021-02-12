2dec6b68122e03a1f16e03ecbec2d92689de88a5
Daniel Weindl <dweindl@users.noreply.github.com>
2021-02-12 15:00:43 +0100

    Update to Ceres 2.0.0 (#316)
    and cleanup build config

--- buildAll.sh
+++ buildAll.sh
@@ -16,17 +16,12 @@ cd "${amici_dir}"
 "${parpe_root}/ThirdParty/installIpopt.sh"
 
 # build parpe
-ceres_install_dir="${parpe_root}/ThirdParty/ceres-solver-1.13.0/build/install"
-ceres_libs="${ceres_install_dir}/lib/libceres.a;$(pkg-config --libs blas);cxsparse;lapack;cholmod;camd;colamd"
-ceres_inc="${ceres_install_dir}/include/"
-ceres_inc="${ceres_inc};${parpe_root}/ThirdParty/eigen-eigen-67e894c6cd8f/build/install/include/eigen3/"
+export PKG_CONFIG_PATH=${parpe_root}/ThirdParty/Ipopt-releases-3.13.3/:${PKG_CONFIG_PATH:-}
 parpe_build_dir="${parpe_root}/build"
 mkdir -p "${parpe_build_dir}"
 cd "${parpe_build_dir}"
 cmake \
       -DIPOPT_DIR="${parpe_root}/ThirdParty/Ipopt-3.12.12/install" \
-      -DCERES_LIBRARIES="${ceres_libs}" \
-      -DCERES_INCLUDE_DIRS="${ceres_inc}" \
       "${parpe_root}"
 make -j12
 
