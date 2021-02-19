95a939d607a5c369fcb46ab2d5d513807214b32f
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2021-02-19 21:42:17 +0100

    format

--- src/parpeoptimization/optimizationProblem.cpp
+++ src/parpeoptimization/optimizationProblem.cpp
@@ -129,9 +129,10 @@ void optimizationProblemGradientCheckMultiEps(OptimizationProblem *problem,
             ll = LOGLVL_WARNING;
         if (fabs(regRelErr_best) > 1e-2)
             ll = LOGLVL_ERROR;
-        logmessage(ll, "%5d g: %12.6g  fd_c: %12.6g  |Δ/fd_c|: %.6e  |Δ|: %12.6g"
-                       "  ϵ: %12.6g ",
-                   curInd, curGrad, fd_c_best, regRelErr_best, absErr_best, eps_best);
+        logmessage(ll, "%5d g: %12.6g  fd_c: %12.6g  |Δ/fd_c|: %.6e  "
+                       "|Δ|: %12.6g  ϵ: %12.6g ",
+                   curInd, curGrad, fd_c_best, regRelErr_best, absErr_best,
+                   eps_best);
     }
 }
 
