e30fe2865f641af2e281383124c72f7bc3c9bc65
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2021-03-22 11:01:43 +0100

    CI: disable Fujita_SciSignal2010 for now

--- benchmark_collection/all.sh
+++ benchmark_collection/all.sh
@@ -11,10 +11,10 @@ expected_to_work="
 Boehm_JProteomeRes2014
 Borghans_BiophysChem1997
 Elowitz_Nature2000
-Fujita_SciSignal2010
 Sneyd_PNAS2002
 Zheng_PNAS2012"
-#Schwen_PONE2014 Chen_MSB2009
+# Fujita_SciSignal2010 "Timepoint-specific parameter overrides currently unsupported." in PEtab parameter mapping
+# Schwen_PONE2014 Chen_MSB2009
 
 for MODEL in $expected_to_work; do
   printf '=%.0s' {1..20}
