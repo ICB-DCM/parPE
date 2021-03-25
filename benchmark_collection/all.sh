9d19c08c90db18973a5b1525d6a2f4dec98875d5
Daniel Weindl <dweindl@users.noreply.github.com>
2021-03-25 10:40:17 +0100

    CI: Update benchmark models tests / add new models (#332)
    * Cleanup benchmark model scripts
    
    * Additional models
    
    * require petab>=0.1.18

--- benchmark_collection/all.sh
+++ benchmark_collection/all.sh
@@ -1,28 +1,60 @@
 #!/usr/bin/env bash
 # Import, build, and run benchmark models
 
-set -e
+set -eou pipefail
 
+# Path to benchmark collection model directory
 [[ -n "${BENCHMARK_COLLECTION}" ]] && model_dir="${BENCHMARK_COLLECTION}"
 
 # all_models=$(ls -1d ${model_dir}/*/)
-
 expected_to_work="
 Boehm_JProteomeRes2014
 Borghans_BiophysChem1997
+Crauste_CellSystems2017
 Elowitz_Nature2000
+Fujita_SciSignal2010
+Schwen_PONE2014
 Sneyd_PNAS2002
-Zheng_PNAS2012"
-# Fujita_SciSignal2010 "Timepoint-specific parameter overrides currently unsupported." in PEtab parameter mapping
-# Schwen_PONE2014 Chen_MSB2009
+Weber_BMC2015
+Zheng_PNAS2012
+"
+# Timepoint-specific parameter overrides currently unsupported.:
+#
+# Chen_MSB2009
+#
+# missing ref:
+#
+# Alkan_SciSignal2018
+# Blasi_CellSystems2016
+# Bruno_JExpBio2016
+# Giordano_Nature2020
+# Okuonghae_ChaosSolitonsFractals2020
+# Perelson_Science1996
+# Rahman_MBS2016
+# Raimundez_PCB2020
+# SalazarCavazos_MBoC2020
+# Zhao_QuantBiol2020
+#
+# integration trouble:
+#
+# Beer_MolBioSystems2014
+# Bertozzi_PNAS2020
+# Borghans_BiophysChem1997
+# Isensee_JCB2018
+#
+# Mismatch:
+# Bachmann_MSB2011: Expected llh -478.459689232875, got nllh -418.409381
+# Brannmark_JBC2010 FAILED: Expected llh 283.778227541074, got nllh 141.889034
+# Lucarelli_CellSystems2018
+# Fiedler_BMC2016 FAILED: Expected llh -117.16780323362, got nllh 109391.496205
 
-for MODEL in $expected_to_work; do
+for model_name in $expected_to_work; do
   printf '=%.0s' {1..20}
-  printf %s "${MODEL}"
+  printf %s "${model_name}"
   printf '=%.0s' {1..20}
   echo
-  echo ./import_and_run.sh "${model_dir}"/"${MODEL}"
-  ./import_and_run.sh "${model_dir}"/"${MODEL}"
+  echo ./import_and_run.sh "${model_dir}"/"${model_name}"
+  ./import_and_run.sh "${model_dir}"/"${model_name}"
   printf '=%.0s' {1..100}
   echo
 done
