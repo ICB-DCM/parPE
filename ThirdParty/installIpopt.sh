a5cd3e71e0b624bde69476b076184e567f61ab3f
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2021-03-27 11:48:21 +0100

    Fix Fix/update IpOpt download script for better handling of libcoinhsl

--- ThirdParty/installIpopt.sh
+++ ThirdParty/installIpopt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# build Ipopt
+# Download and build Ipopt and HSL
 
 set -euo pipefail
 script_dir=$(dirname "$0")
@@ -16,6 +16,7 @@ hsl_install_dir="${ipopt_dir}/ThirdParty-HSL/install"
 
 if [[ ! -d "${ipopt_dir}" ]]; then
   if [[ ! -f "${ipopt_archive}" ]]; then
+    echo "Downloading IpOpt source archive ..."
     wget -O "${ipopt_archive}" "${ipopt_url}"
   else
     echo "Skipping download step."
@@ -26,42 +27,54 @@ if [[ ! -d "${ipopt_dir}" ]]; then
   # Handle HSL solvers
   cd "${ipopt_dir}"
   if [[ ! -d "ThirdParty-HSL" ]]; then
+    echo "Cloning ThirdParty-HSL ..."
     git clone https://github.com/coin-or-tools/ThirdParty-HSL.git
   fi
 
   cd ThirdParty-HSL
 
+  # Need some coinhsl library.
   if [[ ! -d "coinhsl" ]]; then
-    # Need some coinhsl library. Check for common ones:
-    if [[ -f "${script_dir}/coinhsl-2015.06.23.tar.gz" ]]; then
-      tar -xzf "${script_dir}/coinhsl-2015.06.23.tar.gz"
-      mv coinhsl-2015.06.23 coinhsl
-    elif [[ -f "${script_dir}/coinhsl-2014.01.10.tar.gz" ]]; then
-      tar -xzf "${script_dir}/coinhsl-2014.01.10.tar.gz"
-      mv coinhsl-2014.01.10 coinhsl
-    elif [[ -f "${script_dir}/coinhsl-2019.05.21.tar.gz" ]]; then
-      tar -xzf "${script_dir}/coinhsl-2019.05.21.tar.gz"
-      mv coinhsl-2019.05.21 coinhsl
+    # ThirdParty/coinhsl ?
+    if [[ -d "${script_dir}/coinhsl" ]]; then
+      echo "Using coinhsl from ${script_dir}/coinhsl"
+      cp -aR "${script_dir}/coinhsl" coinhsl
     else
-      echo "Did not find coinhsl/ or a known coinhsl archive."
-      echo "Name your coinhsl version."
-      read version
-      if [[ -f "${script_dir}/coinhsl-${version}.tar.gz" ]]; then
-      	tar -xzf "${script_dir}/coinhsl-${version}.tar.gz"
-      	mv coinhsl-${version} coinhsl
-      else
-      	echo "Press any key to continue"
-      	read -n 1 -s -r
-      fi
+      # Check for common ones:
+      coinhsl_archive_names="
+      coinhsl-2014.01.10
+      coinhsl-2015.06.23
+      coinhsl-2019.05.21
+      "
+      for coinhsl_archive in $coinhsl_archive_names; do
+        if [[ -f "${script_dir}/${coinhsl_archive}.tar.gz" ]]; then
+          echo "Unpacking ${script_dir}/${coinhsl_archive}.tar.gz ..."
+          tar -xzf "${script_dir}/${coinhsl_archive}.tar.gz"
+          mv "${coinhsl_archive}" coinhsl
+        fi
+      done
+    fi
+
+    if [[ ! -d "coinhsl" ]]; then
+      echo "Did not find coinhsl/ or a known coinhsl archive." \
+      "IpOpt will probably not work."
     fi
   fi
 
-  ./configure --prefix="${hsl_install_dir}"
+  # Use Intel MKL for lapack?
+  lapack_lflags=""
+  if [[ -v MKL_LIB ]]; then
+    lapack_lflags="--with-lapack-lflags=${MKL_LIB}"
+  fi
+
+  ./configure --prefix="${hsl_install_dir}" --with-lapack "${lapack_lflags}"
   make
   make install
-  (cd ${hsl_install_dir}/lib && test ! -e libhsl.so && ln -s libcoinhsl.so libhsl.so || true)
+  # For some versions of HSL, the library may have the wrong name
+  (cd "${hsl_install_dir}/lib" && test ! -e libhsl.so && ln -s libcoinhsl.so libhsl.so || true)
 
   cd "${ipopt_dir}"
+
   ./configure --prefix="${ipopt_install_dir}" \
     --enable-static \
     --disable-shared \
@@ -69,7 +82,8 @@ if [[ ! -d "${ipopt_dir}" ]]; then
     --with-hsl \
     --with-hsl-cflags=-I${hsl_install_dir}/include/coin-or/hsl \
     --with-hsl-lflags="-L${hsl_install_dir}/lib/ -lcoinhsl" \
-    --disable-linear-solver-loader
+    --disable-linear-solver-loader \
+    --with-lapack "${lapack_lflags}"
   make $make_opts
   make $make_opts install
 else
