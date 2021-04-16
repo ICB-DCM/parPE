f61d770a20d2a305c7a2e00d9f53856e4450ee49
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2021-04-16 16:53:53 +0200

    blaze

--- ThirdParty/installFides.sh
+++ ThirdParty/installFides.sh
@@ -9,6 +9,20 @@ cd "${script_dir}"
 make_opts=${MAKEOPTS-}
 
 fides_dir=${script_dir}/fides-cpp
+blaze_dir=${script_dir}/blaze-3.8
+
+cd "${script_dir}"
+
+if [[ ! -d "${blaze_dir}" ]]; then
+  if [[ ! -f "blaze-3.8.tar.gz" ]]; then
+    wget https://bitbucket.org/blaze-lib/blaze/downloads/blaze-3.8.tar.gz
+  fi
+  tar -xzf blaze-3.8.tar.gz
+  cd blaze-3.8 \
+  cmake -S . -B build/ -DCMAKE_INSTALL_PREFIX=/usr/local/
+  cd build
+  sudo make -j2 install
+fi
 
 if [ ! -d "${fides_dir}" ]; then
   cd "${script_dir}" && git clone https://github.com/dweindl/fides-cpp.git
