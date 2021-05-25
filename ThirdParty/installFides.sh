8d7b3af4ef78103e56dc7b5f6c8f0ed1c8feab33
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2021-05-25 16:31:10 +0200

    cleanup

--- ThirdParty/installFides.sh
+++ ThirdParty/installFides.sh
@@ -1,6 +1,35 @@
 #!/usr/bin/env bash
 # Download and build fides-cpp
 
+build_blaze() {
+  cd "${script_dir}"
+
+  if [[ ! -d "${blaze_dir}" ]]; then
+    if [[ ! -f "blaze-3.8.tar.gz" ]]; then
+      wget https://bitbucket.org/blaze-lib/blaze/downloads/blaze-3.8.tar.gz
+    fi
+    tar -xzf blaze-3.8.tar.gz
+    cd blaze-3.8
+    cmake -S . -B build/ -DCMAKE_INSTALL_PREFIX="$(pwd)/build/install"
+    cd build
+    make -j2 install
+  fi
+}
+
+build_fides() {
+  if [ ! -d "${fides_dir}" ]; then
+    cd "${script_dir}"
+    git clone https://github.com/dweindl/fides-cpp.git
+    cd "${fides_dir}"
+    git checkout develop
+  fi
+
+  cd "${fides_dir}"
+  cmake -S . -B build -DBUILD_TESTING=OFF
+  cd build
+  make ${make_opts}
+}
+
 set -euo pipefail
 script_dir=$(dirname "$0")
 script_dir=$(cd "${script_dir}" && pwd)
@@ -11,27 +40,5 @@ make_opts=${MAKEOPTS-}
 fides_dir=${script_dir}/fides-cpp
 blaze_dir=${script_dir}/blaze-3.8
 
-cd "${script_dir}"
-
-if [[ ! -d "${blaze_dir}" ]]; then
-  if [[ ! -f "blaze-3.8.tar.gz" ]]; then
-    wget https://bitbucket.org/blaze-lib/blaze/downloads/blaze-3.8.tar.gz
-  fi
-  tar -xzf blaze-3.8.tar.gz
-  cd blaze-3.8
-  cmake -S . -B build/ -DCMAKE_INSTALL_PREFIX=/usr/local/
-  cd build
-  sudo make -j2 install
-fi
-
-if [ ! -d "${fides_dir}" ]; then
-  cd "${script_dir}"
-  git clone https://github.com/dweindl/fides-cpp.git
-  cd "${fides_dir}"
-  git checkout develop
-fi
-
-cd "${fides_dir}"
-cmake -S . -B build -DBUILD_TESTING=OFF
-cd build
-make ${make_opts}
+build_blaze
+build_fides
