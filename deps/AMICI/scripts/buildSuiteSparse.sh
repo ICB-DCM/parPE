ecb92c477e12c34202ef11c8b377594692eecaeb
Daniel Weindl <daniel.weindl@uni-bonn.de>
2024-10-11 08:58:31 +0200

    git subrepo clone --branch=f5533ba16a320aa968877112c2f6395f5d2c4741 --force git@github.com:dweindl/AMICI.git deps/AMICI
    subrepo:
      subdir:   "deps/AMICI"
      merged:   "f5533ba1"
    upstream:
      origin:   "git@github.com:dweindl/AMICI.git"
      branch:   "f5533ba16a320aa968877112c2f6395f5d2c4741"
      commit:   "f5533ba1"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"

--- deps/AMICI/scripts/buildSuiteSparse.sh
+++ deps/AMICI/scripts/buildSuiteSparse.sh
@@ -10,7 +10,17 @@ amici_path=$(cd "$script_path/.." && pwd)
 suitesparse_root="${amici_path}/ThirdParty/SuiteSparse"
 
 cd "${suitesparse_root}/build"
+
+if [[ "${GITHUB_REPOSITORY:-}" = *"/AMICI" ]] ||
+  [ "${ENABLE_AMICI_DEBUGGING:-}" = TRUE ]; then
+  # Running on CI server
+  build_type="Debug"
+else
+  build_type="RelWithDebInfo"
+fi
+
 cmake -DSUITESPARSE_ENABLE_PROJECTS="amd;btf;colamd;klu" \
+  -DCMAKE_BUILD_TYPE="$build_type" \
   -DCMAKE_INSTALL_PREFIX="${suitesparse_root}/install" \
   -DCHOLMOD_CAMD=OFF \
   -DKLU_USE_CHOLMOD=OFF \
@@ -22,6 +32,8 @@ cmake -DSUITESPARSE_ENABLE_PROJECTS="amd;btf;colamd;klu" \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
   -DSUITESPARSE_USE_64BIT_BLAS=ON \
   -DBLAS_LIBRARIES=dummy \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_TESTING=OFF \
   ..
 
 cmake --build .
